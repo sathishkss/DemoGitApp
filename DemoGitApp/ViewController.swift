e9e6bbce487c26b2e3d72a9fa89afc329fe05e82
sasedeveMac <sasedeveMac@sasedeveMacs-Mac-mini-2.local>
2019-11-07 16:27:25 +0530

    Hello world

--- DemoGitApp/ViewController.swift
+++ DemoGitApp/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World")
+        
+        //https://github.com/sathishkss/DemoGitApp.git
+        
     }
 
     override func didReceiveMemoryWarning() {
